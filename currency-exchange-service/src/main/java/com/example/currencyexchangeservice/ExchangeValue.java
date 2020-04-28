f8591b44fa6f0b93c753da1b7f9986b594b93ca3
Carlos Lozano <carlosmlozanos@gmail.com>
2020-04-28 20:18:07 +0200

    step 16 - Configure JPA and initializae database

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/ExchangeValue.java
@@ -2,6 +2,11 @@ package com.example.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +14,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class ExchangeValue {
+	@Id
+	@GeneratedValue
 	private Long id;
+	@Column(name="currency_from")
 	private String from;
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private int port;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values(10001, 'USD', 'EUR', 65, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values(10002, 'EUR', 'USD', 120, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values(10003, 'USD', 'INR', 30, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values(10004, 'EUR', 'INR', 40, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port) values(10005, 'INR', 'EUR', 150, 0);
