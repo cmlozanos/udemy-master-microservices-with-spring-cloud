2f47fe7fa297aac794f29dc00f8fba33a2ee23c6
Carlos Lozano <carlosmlozanos@gmail.com>
2020-05-04 17:44:34 +0200

    Setting up Spring Netflix Hystrix for fault tolerance

--- limits-service/src/main/java/com/example/limitsservice/LimistConfigurationController.java
+++ limits-service/src/main/java/com/example/limitsservice/LimistConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimistConfigurationController {
 
@@ -13,5 +15,15 @@ public class LimistConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMax(), configuration.getMin());
+	}
+	
+	@GetMapping("/limits-fault-tolerance")
+	@HystrixCommand(fallbackMethod = "retrieveLimitsFromConfigurationsFaultTolerance")
+	public LimitConfiguration retrieveLimitsFromConfigurationsFailed() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration retrieveLimitsFromConfigurationsFaultTolerance() {
+		return new LimitConfiguration(9, 999);
 	}
 }
